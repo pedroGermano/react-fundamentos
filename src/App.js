5288f2bdd21608880275398549c326cd5ce371ad
Pedro Germano <pedrogermano232@gmail.com>
2022-09-05 07:55:20 -0300

    Render list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Post from "./Post";
 
+const posts = [
+  { title: 'title#01', subtitle: 'Sub#01', likes: 20},
+  { title: 'title#02', subtitle: 'Sub#02', likes: 10},
+  { title: 'title#03', subtitle: 'Sub#03', likes: 50}
+]
+
+
 function App() {
   return (
     <>
@@ -11,29 +18,16 @@ function App() {
 
       <hr />
 
-      <Post
-        likes={20}
-        post={{
-          title: "Título da notícia 01",
-          subtitle: "Subtítulo da notícia 01",
-        }}
-      />
-
-      <Post
-        likes={20}
-        post={{
-          title: "Título da notícia 02",
-          subtitle: "Subtítulo da notícia 02",
-        }}
-      />
-
-      <Post
-        likes={20}
-        post={{
-          title: "Título da notícia 03",
-          subtitle: "Subtítulo da notícia 03",
-        }}
-      />
+      {posts.map(post => (
+        <Post
+          key={post.title}
+          likes={post.likes}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle,
+          }}
+        />
+      ))}
     </>
   );
 }
